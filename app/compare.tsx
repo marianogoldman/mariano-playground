b546f21b12385407d59f91a26a660713ce67cd18
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2024-01-30 18:51:01 -0300

    better use of network

--- app/compare.tsx
+++ app/compare.tsx
@@ -19,7 +19,7 @@ const problematicEntityIds: string[] = [
   'bafkreihmn6teertrob5praqvtkc6hall63gqvpqd3numjzuylyywzmekpm'
 ]
 
-const catalystBaseUrl = 'https://peer-ec2.decentraland.org'
+const catalystBaseUrl = 'https://peer.decentraland.org'
 const cdnBaseUrl = 'https://profile-images-bucket-43d0c58.decentraland.org/v1/entities'
 
 type EntityData = {
@@ -27,16 +27,7 @@ type EntityData = {
   address: string
   body: string | undefined
   face: string | undefined
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function printEntityId(entityId: string): string {
-  return entityId.slice(0, 10) + '...' + entityId.slice(-10)
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function printAddress(address: string): string {
-  return address.slice(0, 7) + '...' + address.slice(-5)
+  raw: any
 }
 
 function uniqBy<T>(a: T[], key: (item: any) => string) {
@@ -47,91 +38,65 @@ function uniqBy<T>(a: T[], key: (item: any) => string) {
   })
 }
 
-async function fetchProfiles(entityIds: string[]): Promise<EntityData[]> {
-  console.log('fetching profiles', entityIds)
-  return fetch(
-    `${catalystBaseUrl}/content/deployments?` +
-      new URLSearchParams([
-        ['entityType', 'profile'],
-        ['from', '1706497200000'],
-        ...entityIds.map((entityId) => ['entityId', entityId])
-      ])
-  )
-    .then((response) => response.json())
-    .then((data) =>
-      data.deployments.map((entity: any) => ({
-        id: entity.entityId,
-        address: entity.pointers[0],
-        body: entity.metadata.avatars[0].avatar.snapshots.body,
-        face: entity.metadata.avatars[0].avatar.snapshots.face256
-      }))
-    )
-    .catch((err) => {
-      console.error(err)
-      return []
-    })
-}
-
 export default function Compare() {
-  const [initialEntities, setInitialEntities] = useState<string[]>(problematicEntityIds)
-  const [editingEntities, setEditingEntities] = useState<string[]>(problematicEntityIds)
-  const [count, setCount] = useState<number>(10)
+  const [editingEntities, setEditingEntities] = useState<string[]>([])
   const [entities, setEntities] = useState<EntityData[]>([])
   const [openBox, setOpenBox] = useState<boolean>(false)
 
-  function loadRecentProfiles() {
-    fetch(`${catalystBaseUrl}/content/deployments?entityType=profile&from=1706497200000`)
+  function loadProfiles(entityIds: string[] = []) {
+    fetch(
+      `${catalystBaseUrl}/content/deployments?` +
+        new URLSearchParams([
+          ['entityType', 'profile'],
+          ['from', '1706497200000'],
+          ['to', `${Date.now() - 2 * 60 * 1000}`],
+          ['limit', '50'],
+          ...entityIds.map((entityId) => ['entityId', entityId])
+        ])
+    )
       .then((response) => response.json())
-      .then((data) =>
-        loadProfilesIds(
-          uniqBy(
-            data.deployments.map((deployment: any) => deployment.entityId),
-            (a: string) => a
-          )
-        )
+      .then(
+        (data): Promise<EntityData[]> =>
+          data.deployments.map((entity: any) => ({
+            id: entity.entityId,
+            address: entity.pointers[0],
+            body: entity.metadata.avatars[0].avatar.snapshots.body,
+            face: entity.metadata.avatars[0].avatar.snapshots.face256,
+            raw: entity
+          }))
       )
+      .then((data) => setEntities(uniqBy(data, (a: EntityData) => a.id)))
       .catch(console.error)
-  }
-
-  function loadProfilesIds(entityIds: string[]) {
-    setInitialEntities(uniqBy(entityIds, (a: string) => a))
-    setEntities([])
-    setCount(10)
   }
 
   useEffect(() => {
-    fetchProfiles(initialEntities.slice(count - 10, count))
-      .then((data) => {
-        console.log('fetched profiles', data)
-        setEntities((entities) => uniqBy([...entities, ...data], (a: EntityData) => a.id))
-      })
-      .catch(console.error)
-  }, [initialEntities, count])
+    loadProfiles(problematicEntityIds)
+  }, [])
 
   return (
     <div className="bg-white">
       <h1 className="text-center text-4xl font-bold text-gray-600 my-4">Compare images</h1>
       <div className="flex justify-between">
         <div className="m-2 text-gray-500">
-          Displaying {entities.length} profiles of {count}
+          Displaying {entities.length} profiles of {entities.length}
         </div>
         <div className="flex justify-between gap-2">
           <button
             className="h-8 rounded bg-indigo-600 px-2 py-1 font-mono text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            onClick={() => loadRecentProfiles()}
+            onClick={() => loadProfiles()}
           >
             Recent deployments
           </button>
           <button
             className="h-8 rounded bg-indigo-600 px-2 py-1 font-mono text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            onClick={() => loadProfilesIds(problematicEntityIds)}
+            onClick={() => loadProfiles(problematicEntityIds)}
           >
             Profiles with issues
           </button>
           <button
             className="h-8 rounded bg-indigo-600 px-2 py-1 font-mono text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             onClick={() => {
-              setEditingEntities(initialEntities)
+              setEditingEntities(entities.map((e) => e.id))
               setOpenBox(!openBox)
             }}
           >
@@ -146,16 +111,14 @@ export default function Compare() {
             name="comment"
             id="comment"
             className="block w-full font-mono rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            defaultValue={initialEntities.join('\n')}
+            defaultValue={entities.map((e) => e.id).join('\n')}
             onChange={(e) => setEditingEntities(e.target.value.split('\n'))}
           />
           <div className="text-center m-2">
             <button
               className="h-8 rounded bg-indigo-600 px-2 py-1 font-mono text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               onClick={() => {
-                setInitialEntities(editingEntities)
-                setEntities([])
-                setCount(10)
+                loadProfiles(editingEntities)
                 setOpenBox(false)
               }}
             >
@@ -214,18 +177,6 @@ export default function Compare() {
             </div>
           </div>
         ))}
-      </div>
-      <div className="text-center">
-        {count < initialEntities.length && (
-          <button
-            className="h-8 rounded bg-indigo-600 px-2 py-1 font-mono text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            onClick={() => {
-              setCount((count) => count + 10)
-            }}
-          >
-            Load more
-          </button>
-        )}
       </div>
     </div>
   )
