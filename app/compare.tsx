55b93bec0d8d0d11a658cca0ad2586398ae1d199
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2024-01-31 10:57:23 -0300

    view wearables

--- app/compare.tsx
+++ app/compare.tsx
@@ -2,6 +2,7 @@
 
 import { Fragment, useEffect, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
 
 const problematicEntityIds: string[] = [
   'bafkreiarkmiqdnx7sydzf27gprlt6cxxpwy2373c4rklwe4nlrzlmbgs7a',
@@ -42,7 +43,9 @@ function uniqBy<T>(a: T[], key: (item: any) => string) {
 export default function Compare() {
   const [editingEntities, setEditingEntities] = useState<string[]>([])
   const [entities, setEntities] = useState<EntityData[]>([])
-  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false)
+  const [slideOverOpen, setSlideOver] = useState<EntityData | null>(null)
+  const [selectedEntity, setSelectedEntity] = useState<EntityData | null>(null)
 
   function loadProfiles(entityIds: string[] = []) {
     fetch(
@@ -98,15 +101,17 @@ export default function Compare() {
             className="h-8 rounded bg-indigo-600 px-2 py-1 font-mono text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             onClick={() => {
               setEditingEntities(entities.map((e) => e.id))
-              setIsOpen(!isOpen)
+              setIsDialogOpen(!isDialogOpen)
             }}
           >
             Specific profiles
           </button>
         </div>
       </div>
-      <Transition.Root show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={setIsOpen}>
+
+      {/* Form Dialog */}
+      <Transition.Root show={isDialogOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setIsDialogOpen}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -150,7 +155,7 @@ export default function Compare() {
                       className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                       onClick={() => {
                         loadProfiles(editingEntities)
-                        setIsOpen(false)
+                        setIsDialogOpen(false)
                       }}
                     >
                       Load set
@@ -163,11 +168,76 @@ export default function Compare() {
         </Dialog>
       </Transition.Root>
 
+      {/* Slide over */}
+      <Transition.Root show={slideOverOpen !== null} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={() => setSlideOver(null)}>
+          <div className="fixed inset-0" />
+
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-500 sm:duration-700"
+                  enterFrom="translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transform transition ease-in-out duration-500 sm:duration-700"
+                  leaveFrom="translate-x-0"
+                  leaveTo="translate-x-full"
+                >
+                  <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                    <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                      <div className="px-4 sm:px-6">
+                        <div className="flex items-start justify-between">
+                          <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
+                            Wearables
+                          </Dialog.Title>
+                          <div className="ml-3 flex h-7 items-center">
+                            <button
+                              type="button"
+                              className="relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                              onClick={() => setSlideOver(null)}
+                            >
+                              <span className="absolute -inset-2.5" />
+                              <span className="sr-only">Close panel</span>
+                              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                        <div className="text-center mb-3">
+                          <div className="overflow-hidden h-full w-full">
+                            <img
+                              className="object-cover border w-24 h-48"
+                              src={`${catalystBaseUrl}/content/contents/${slideOverOpen?.body}`}
+                              alt=""
+                            />
+                          </div>
+                        </div>
+                        <ol className="grid grid-cols-1 gap-1">
+                          {slideOverOpen?.raw.metadata.avatars[0].avatar.wearables.map((wearable: string) => (
+                            <li className="p-1 bg-white text-sm border">{wearable}</li>
+                          ))}
+                        </ol>
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+
       <div className="text-sm m-4">
         {entities.map((entityData) => (
           <div key={entityData.id} className="mt-6 grid gap-x-2 grid-cols-4 text-gray-500 border-2 p-2">
             <div className="col-span-4 text-xs text-center">
-              {entityData.id} -{' '}
+              <a href="#" onClick={() => setSlideOver(entityData)} className="text-indigo-600">
+                {entityData.id}
+              </a>{' '}
+              -{' '}
               <a
                 href={`https://decentraland.org/profile/accounts/${entityData.address}`}
                 target="_blank"
